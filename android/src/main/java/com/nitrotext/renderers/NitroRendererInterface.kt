3e10b323bf06cb12ba900c5c76657aae8294dc1b
Patrick Kabwe <patrickckabwe@gmail.com>
2025-11-23 21:30:37 +0200

    feat: add HTML rendering logic support
    - Remove old renderer system (NitroRenderer, RichTextStyle, RichTextStyleRule)
    - Add new Renderer type for HTML and plaintext rendering
    - Update renderer prop support in native implementations
    - HTML parsing is now handled in JS/TS layer
    - Add renderer bridge code for Android

--- android/src/main/java/com/nitrotext/renderers/NitroRendererInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.nitrotext.renderers
-
-import android.text.SpannableStringBuilder
-import com.margelo.nitro.nitrotext.RichTextStyle
-import com.margelo.nitro.nitrotext.RichTextStyleRule
-
-interface NitroRendererInterface {
-  fun render(input: String, baseStyle: RichTextStyle?, rules: Array<RichTextStyleRule>?): SpannableStringBuilder
-}
